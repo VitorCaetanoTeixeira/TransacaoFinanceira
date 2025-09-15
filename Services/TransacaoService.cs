e6596d7fea5eb77cf18fcc632039b76899cfa209
Rafael Caetano Teixeira <rafa.teirico@gmail.com>
2025-09-15 00:23:23 -0300

    Correção e separação de arquivos

--- Services/TransacaoService.cs
+++ Services/TransacaoService.cs
@@ -13,21 +13,21 @@ namespace TransacaoFinanceira.Services
         }
 
         public void Transferir(Operacao operacao){
-            var origem = _contas.GetById(operacao.contaOrigem);
-            var destino =  _contas.GetById(operacao.contaDestino);
+            var contaOrigem = _contas.GetById(operacao.contaOrigem);
+            var contaDestino =  _contas.GetById(operacao.contaDestino);
             if(operacao.valor <= 0){
                 Console.WriteLine($"Nào é possivel fazer transferencias com valor menor ou igual a zero");
                 return;
             }
 
-            if(!origem.Debitar(operacao.valor)){
+            if(!contaOrigem.Debitar(operacao.valor)){
                 
                 Console.WriteLine($"Transacao número {operacao.correlationId} foi cancelada por falta de saldo");
                 return;
             }
 
-            destino.Creditar(operacao.valor);
-            Console.WriteLine($"Transacao número {operacao.correlationId} foi efetuada com sucesso!! Saldo atual conta Origem:{origem.saldo} Saldo atual conta Destino:{destino.saldo}");
+            contaDestino.Creditar(operacao.valor);
+            Console.WriteLine($"Transacao número {operacao.correlationId} foi efetuada com sucesso!! Novos saldos: Conta Origem: {contaOrigem.saldo} | Conta Destino: {contaDestino.saldo}");
                
 
         }
